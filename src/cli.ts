e68825d2c60a27195ecc63038278e8f2fe66a997
Huafu Gandon <huafu.gandon@gmail.com>
2022-11-26 17:44:02 +0100

    adds init command and improves compile

--- src/cli.ts
+++ src/cli.ts
@@ -19,7 +19,7 @@ const COMPILER_OPTIONS: ts.CompilerOptions = {
 	typeRoots: []
 };
 
-export function compile(sourceDir: string, targetDir = `${sourceDir}/out/`): void {
+export function compile(sourceDir: string, targetDir: string): void {
 	const cfg = ts.parseJsonConfigFileContent(
 		{
 			compilerOptions: <ts.CompilerOptions>{
@@ -65,8 +65,45 @@ export function compile(sourceDir: string, targetDir = `${sourceDir}/out/`): voi
 	process.exit(exitCode);
 }
 
+export const init = () => {
+	const dir = process.cwd();
+	fs.copyFileSync(
+		path.resolve(__dirname, '..', 'ucode', 'package.template.json'),
+		path.resolve(dir, 'package.json')
+	);
+	fs.copyFileSync(
+		path.resolve(__dirname, '..', 'ucode', 'tsconfig.template.json'),
+		path.resolve(dir, 'tsconfig.json')
+	);
+	console.log(
+		"Don't forget to run `npm install` so that you get typings and good completion in your IDE"
+	);
+};
+
 export const run = () => {
-	compile(process.argv[2] || process.cwd(), process.argv[3]);
+	const args = process.argv.slice(2);
+
+	if (args.length === 1 && args[0] === 'init') {
+		return init();
+	}
+
+	const cwd = process.cwd();
+	const firstArgIsDir =
+		args[0]?.length > 0 && fs.existsSync(args[0]) && fs.statSync(args[0]).isDirectory();
+	const root = path.resolve(firstArgIsDir ? args[0] : args[0] ? path.dirname(args[0]) : cwd);
+	let srcDir = args[0] ? root : undefined;
+	const tsconfigFile = path.resolve(root, 'tsconfig.json');
+	let defaultDstDir = path.resolve(root, 'out');
+	let defaultSrcDir: string = root;
+
+	if (fs.existsSync(tsconfigFile)) {
+		// eslint-disable-next-line @typescript-eslint/no-var-requires
+		const options = require(tsconfigFile).compilerOptions as ts.CompilerOptions | undefined;
+		if (options?.rootDir) defaultSrcDir = path.resolve(root, options.rootDir);
+		if (options?.outDir) defaultDstDir = path.resolve(root, options.outDir);
+	}
+
+	compile(srcDir ?? defaultSrcDir, args[1] ?? defaultDstDir);
 };
 
 if (require.main === module) run();
